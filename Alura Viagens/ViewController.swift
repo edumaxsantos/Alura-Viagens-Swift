7b75a8d79d4fc7def9f00e18b45c46b2ed7d78e0
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-06-07 21:41:59 -0300

    Adicionado tabela básica

--- Alura Viagens/ViewController.swift
+++ Alura Viagens/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tabelaViagens: UITableView!
+    let listaViagens = ["Rio de Janeiro", "Ceará", "São Paulo"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tabelaViagens.dataSource = self
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = listaViagens[indexPath.row]
+        return cell
     }
 
 
