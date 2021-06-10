3dc113a44efa156adc4f2cd1156c346950d127fb
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-06-09 23:54:52 -0300

    Células agora possuem layout mais adequado, aparecendo as propriedades

--- Alura Viagens/Pacotes/Controller/PacotesViagensViewController.swift
+++ Alura Viagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -13,11 +13,15 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
     
     @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
     
+    // MARK: - Propriedades
+    
+    let listaViagens: [Viagem] = ViagemDAO().retornaTodasAsViagens()
+    
     // MARK: - Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        colecaoPacotesViagem.dataSource = self
+        self.colecaoPacotesViagem.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -25,13 +29,28 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
     // MARK: - DataSource
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return self.listaViagens.count
     }
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
         
-        celulaPacote.backgroundColor = UIColor.blue
+        
+        let viagemAtual = listaViagens[indexPath.item]
+        
+        print(celulaPacote.frame.size)
+        
+        celulaPacote.labelTitulo.text = viagemAtual.titulo
+        celulaPacote.labelPreco.text = "R$ \(viagemAtual.preco)"
+        celulaPacote.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        
+        celulaPacote.layer.borderWidth = 0.5
+        celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
+        
+        celulaPacote.layer.cornerRadius = 8
         
         return celulaPacote
     }
