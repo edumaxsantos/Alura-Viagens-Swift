985fac02e67c4d59f2eb7bc13c7c7609c9e83724
edumaxsantos@gmail.com <edumaxsantos@gmail.com>
2021-06-26 23:38:44 -0300

    Adicionado animação de chacoalhar o UITextField

--- Alura Viagens/Model/Validador.swift
+++ Alura Viagens/Model/Validador.swift
@@ -12,10 +12,29 @@ class Validador: NSObject {
     func validaTextFields(_ textFields: [UITextField?]) -> Bool {
         for textField in textFields {
             if let text = textField?.text, text.isEmpty {
+                chacoalhar(textField)
                 return false
             }
         }
         return true
     }
 
+    func chacoalhar(_ textField: UITextField?) {
+        guard let textField = textField else { return }
+        let chacoalhar = CABasicAnimation(keyPath: "position")
+        
+        chacoalhar.duration = 0.1
+        chacoalhar.repeatCount = 2
+        chacoalhar.autoreverses = true
+        
+        let posicaoInicial = CGPoint(x: textField.center.x + 5, y: textField.center.y)
+        chacoalhar.fromValue = posicaoInicial
+        
+        let posicaoFinal = CGPoint(x: textField.center.x - 5, y: textField.center.y)
+    
+        chacoalhar.toValue = posicaoFinal
+        
+        textField.layer.add(chacoalhar, forKey: nil)
+    }
+    
 }
